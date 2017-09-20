bc82dc82e54a89015577d5c9ece44cc6ea397bbe
zawzaww <mczawzaw12@outlook.com>
2017-09-20 17:03:07 +0630

    TWRP: changed twrp flags

--- BoardConfigOmni.mk
+++ BoardConfigOmni.mk
@@ -1,12 +1,21 @@
 # Inline kernel
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 TARGET_KERNEL_SOURCE := kernel/lge/bullhead
-TARGET_KERNEL_CONFIG := omni_bullhead_defconfig
-
+TARGET_KERNEL_CONFIG := bullhead_defconfig
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_HEADER_ARCH := arm64
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
 TARGET_USES_UNCOMPRESSED_KERNEL := true
+
+# TWRP
+TW_THEME := portrait_hdpi
+RECOVERY_GRAPHICS_USE_LINELENGTH := true
+BOARD_SUPPRESS_SECURE_ERASE := true
+RECOVERY_SDCARD_ON_DATA := true
+BOARD_HAS_NO_REAL_SDCARD := true
+TW_INCLUDE_CRYPTO := true
+TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
+TARGET_RECOVERY_QCOM_RTC_FIX := true
 
 # Use device's audio_effects.conf
 TARGET_USE_DEVICE_AUDIO_EFFECTS_CONF := true
@@ -19,13 +28,3 @@ BOARD_NEEDS_VENDORIMAGE_SYMLINK := true
 
 # Keymaster - Wait for qseecom to load
 TARGET_KEYMASTER_WAIT_FOR_QSEE := true
-
-# TWRP
-TW_THEME := portrait_hdpi
-RECOVERY_GRAPHICS_USE_LINELENGTH := true
-BOARD_SUPPRESS_SECURE_ERASE := true
-RECOVERY_SDCARD_ON_DATA := true
-BOARD_HAS_NO_REAL_SDCARD := true
-TW_INCLUDE_CRYPTO := true
-TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
-TARGET_RECOVERY_QCOM_RTC_FIX := true
