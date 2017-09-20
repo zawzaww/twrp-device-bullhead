56f115f6b03979dac47c4c830d6223d921c9dd77
zawzaww <mczawzaw12@outlook.com>
2017-09-20 18:19:53 +0630

    TWRP: add twrp flags and zawzaw addition
     Changes to be committed:
    	modified:   BoardConfigOmni.mk
    
    Signed-off by: ZawZaw <mczawzaw12@outlook.com>

--- BoardConfigOmni.mk
+++ BoardConfigOmni.mk
@@ -15,11 +15,20 @@ TARGET_USERIMAGES_USE_F2FS := true
 TW_THEME := portrait_hdpi
 RECOVERY_GRAPHICS_USE_LINELENGTH := true
 BOARD_SUPPRESS_SECURE_ERASE := true
-RECOVERY_SDCARD_ON_DATA := true
 BOARD_HAS_NO_REAL_SDCARD := true
-TW_INCLUDE_CRYPTO := true
-TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
 TARGET_RECOVERY_QCOM_RTC_FIX := true
+
+# TWRP ZawZaw Addition
+TW_INCLUDE_CRYPTO := true
+RECOVERY_SDCARD_ON_DATA := true
+TW_MAX_BRIGHTNESS := 215
+TW_DEFAULT_BRIGHTNESS := 100
+TARGET_RECOVERY_DEVICE_DIRS := device/lge/bullhead
+TW_EXCLUDE_SUPERSU := true
+TW_NEVER_UNMOUNT_SYSTEM := true
+TW_NO_SCREEN_TIMEOUT := true
+TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
+TW_DEVICE_VERSION := bullhead by ZawZaw
 
 # Use device's audio_effects.conf
 TARGET_USE_DEVICE_AUDIO_EFFECTS_CONF := true
