3f473228a8e843e882302cf095d95b5df8dd2441
Zaw Zaw <mczawzaw12@outlook.com>
2018-10-18 07:33:48 +0630

    BoardConfigOmni: Update local kernel configuration
    Signed-off-by: Zaw Zaw <mczawzaw12@outlook.com>

--- BoardConfigOmni.mk
+++ BoardConfigOmni.mk
@@ -1,7 +1,7 @@
 # Inline kernel
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 TARGET_KERNEL_SOURCE := kernel/lge/bullhead
-TARGET_KERNEL_CONFIG := purez_defconfig
+TARGET_KERNEL_CONFIG := purez_bullhead_defconfig
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_HEADER_ARCH := arm64
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
